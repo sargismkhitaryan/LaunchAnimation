3000c129b73f3cd6ddd4fef5725f364e1040c688
Sargis Mkhitaryan <sargismk@icloud.com>
2018-09-11 22:31:02 +0400

    first commit

---
+++ Example/LaunchAnimation/LaunchAnimation/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  LaunchAnimation
+//
+//  Created by Sargis on 9/11/18.
+//  Copyright © 2018 Sargis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# Launch Animation
